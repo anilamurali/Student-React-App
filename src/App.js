3fb64670e95d5a6ca0554a98c4b00eb8ad3abc1d
anilamurali <anilamurali98@gmail.com>
2022-05-16 21:10:45 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css' ;
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Register from './component/Register';
+import Studentsearch from './component/Studentsearch';
 
 
 function App() {
   return (
     <>
-    <Register/>
+    <Studentsearch/>
     </>
   );
 }
